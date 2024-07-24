20bad40fcd5afd531e22cfd0a215d98383869bc3
Luis Infante <113048160+LuisMInfante@users.noreply.github.com>
2024-07-24 17:06:16 -0400

    added navbar and routes

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let activePage = '';
+
+	const isActive = (path: string): string => activePage === path ? 'text-primary' : 'text-secondary';
+</script>
+
+<style>
+	.nav-item:hover {
+		text-decoration: underline;
+	}
+</style>
+
+
+<nav class="container mx-auto flex justify-center items-center space-x-8">
+	<ul class="flex justify-center space-x-4">
+		<li><a href="/" class={`nav-item ${isActive('/')}`}>About</a></li>
+		<li><a href="/projects" class={`nav-item ${isActive('/projects')}`}>Projects</a></li>
+		<li><a href="/work" class={`nav-item ${isActive('/work')}`}>Work</a></li>
+	</ul>
+</nav>
+	
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import { onMount } from 'svelte';
+
+	let activePage: string = '';
+
+	onMount(() => {
+		activePage = window.location.pathname;
+	});
 </script>
 
-<slot></slot>
+<div class="min-h-screen flex flex-col">
+	<header class="mt-8">
+		<Navbar {activePage} />
+	</header>
+	
+	<main class="flex-grow">
+		<slot></slot>
+	</main>
+
+</div>
