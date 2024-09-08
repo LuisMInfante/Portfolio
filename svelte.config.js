bee468dcef40f141a8dcf76d74074bf9ed60fb4e
Luis Infante <113048160+LuisMInfante@users.noreply.github.com>
2024-09-08 18:31:20 -0400

    resolved build issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-netlify';
 
 export default {
   preprocess: [vitePreprocess()],
-  kit: {
-	adapter: adapter()
-	}
+  kit: {}
 };
